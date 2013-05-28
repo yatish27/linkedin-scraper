af4035b35e68f36363a9b555912a0ee582922fb6
Justin Grevich <justin@grevi.ch>
2013-05-27 23:59:23 -0700

    fixes Wrong country assigned for US profile. (#14)
    It may be overkill. I used Geocoder to get the country based on the
    locality info that we previously scraped.  I don't see any other option
    other than to remove the country method since I don't believe LinkedIn
    puts country info on public profiles for profiles in the US.

--- lib/linkedin-scraper.rb
+++ lib/linkedin-scraper.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "mechanize"
 require "cgi"
 require "net/http"
+require "geocoder"
 Dir["#{File.expand_path(File.dirname(__FILE__))}/linkedin-scraper/*.rb"].each {|file| require file }
 
 
