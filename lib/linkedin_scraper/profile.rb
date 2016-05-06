09576295c6dc0a9485e8d1017792e844c9a8444c
Oskar Lakner <haerdde@gmail.com>
2016-05-07 00:11:37 +0200

    Fix recent changes in Linkedin DOM and add support for random user agents (#82)
    * fix current_companies
    
    * return company's logo
    
    * oops
    
    * add location
    
    * fix project dates
    
    * return formatted description
    
    * use random user agents

--- lib/linkedin_scraper/profile.rb
+++ lib/linkedin_scraper/profile.rb
@@ -2,25 +2,6 @@
 module Linkedin
   class Profile
 
-    #USER_AGENTS = ["Windows IE 6", "Windows IE 7", "Windows Mozilla", "Mac Safari", "Mac Firefox", "Mac Mozilla", "Linux Mozilla", "Linux Firefox", "Linux Konqueror"]
-    USER_AGENTS = [
-      "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6",
-      "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:5.0) Gecko/20100101 Firefox/5.0",
-      "Mozilla/5.0 (Windows NT 6.1.1; rv:5.0) Gecko/20100101 Firefox/5.0",
-      "Mozilla/5.0 (X11; U; Linux i586; de; rv:5.0) Gecko/20100101 Firefox/5.0",
-      "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Ubuntu/11.04 Chromium/14.0.825.0 Chrome/14.0.825.0 Safari/535.1",
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.824.0 Safari/535.1",
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:5.0) Gecko/20100101 Firefox/5.0",
-      "Mozilla/5.0 (Macintosh; PPC MacOS X; rv:5.0) Gecko/20110615 Firefox/5.0",
-      "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))",
-      "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1; .NET CLR 3.0.04320)",
-      "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)",
-      "Mozilla/5.0 (compatible; Konqueror/4.5; FreeBSD) KHTML/4.5.4 (like Gecko)",
-      "Opera/9.80 (Windows NT 6.1; U; es-ES) Presto/2.9.181 Version/12.00",
-      "Opera/9.80 (X11; Linux x86_64; U; fr) Presto/2.9.168 Version/11.50",
-      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
-      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; da-dk) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1"
-    ]
     ATTRIBUTES = %w(
     name
     first_name
@@ -185,13 +166,13 @@ module Linkedin
     def projects
       @projects ||= @page.search("#projects .project").map do |project|
         p = {}
-        start_date, end_date = project.at("date-range").text.gsub(/\s+|\n/, " ").strip.split(" – ") rescue nil
+        start_date, end_date = project.at(".date-range").text.gsub(/\s+|\n/, " ").strip.split(" – ") rescue nil
 
         p[:title] = project.at(".item-title").text
         p[:link] =  CGI.parse(URI.parse(project.at(".item-title a")['href']).query)["url"][0] rescue nil
         p[:start_date] = parse_date(start_date) rescue nil
         p[:end_date] = parse_date(end_date)  rescue nil
-        p[:description] = project.at(".description").text rescue nil
+        p[:description] = project.at(".description").children().to_s rescue nil
         p[:associates] = project.search(".contributors .contributor").map{ |c| c.at("a").text } rescue nil
         p
       end
@@ -215,10 +196,12 @@ module Linkedin
         company = {}
         company[:title] = node.at(".item-title").text.gsub(/\s+|\n/, " ").strip if node.at(".item-title")
         company[:company] = node.at(".item-subtitle").text.gsub(/\s+|\n/, " ").strip if node.at(".item-subtitle")
+        company[:location] = node.at(".location").text if node.at(".location")
         company[:description] = node.at(".description").text.gsub(/\s+|\n/, " ").strip if node.at(".description")
+        company[:company_logo] = node.at(".logo a img").first[1] if node.at(".logo")
 
-        start_date, end_date = node.at(".meta").text.strip.split(" – ") rescue nil
-        company[:duration] = node.at(".meta").text[/.*\((.*)\)/, 1]
+        start_date, end_date = node.at(".date-range").text.strip.split(" – ") rescue nil
+        company[:duration] = node.at(".date-range").text[/.*\((.*)\)/, 1]
         company[:start_date] = parse_date(start_date) rescue nil
 
         if end_date && end_date.match(/Present/)
@@ -261,7 +244,7 @@ module Linkedin
 
     def http_client
       Mechanize.new do |agent|
-        agent.user_agent = USER_AGENTS.sample
+        agent.user_agent = RandomUserAgent.randomize
         unless @options.empty?
           agent.set_proxy(@options[:proxy_ip], @options[:proxy_port], @options[:username], @options[:password])
         end
